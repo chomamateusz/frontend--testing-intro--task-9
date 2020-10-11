12c51554f2ab54a5d9c498ef9fc2bc2772bca960
Mateusz Choma <kontakt@coderoad.pl>
2020-10-11 18:22:18 +0200

    Tests added for 2. point form TODO

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -5,7 +5,15 @@ describe('Counter class tests', () => {
     it('1. should be a ES6 class', () => {
 
         expect(() => Counter()).toThrow('Cannot call a class as a function')
+        expect(new Counter()).toBeInstanceOf(Counter)
 
+    })
+
+    it('2. should store current number in `number` property of the instance', () => {
+        const counter1 = new Counter()
+
+        expect(counter1.number).toBeDefined()
+        expect(typeof counter1.number).toBe('number')
     })
 
 })
