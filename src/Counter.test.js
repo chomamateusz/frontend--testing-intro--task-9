248a45377a86cb39fa0d7e5ed888b4e483d585b5
Mateusz Choma <kontakt@coderoad.pl>
2020-10-11 20:30:22 +0200

    All method tests added

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,9 +1,9 @@
 import { Counter, DecreasingCounter } from './Counter'
 
-Counter.prototype.render = jest.fn(Counter.prototype.render)
+Counter.prototype.render = jest.fn()
 
 beforeEach(() => {
-    Counter.prototype.render.mockClear()
+    Counter.prototype.render.mockReset()
 })
 
 describe('Counter render', () => {
@@ -26,6 +26,28 @@ describe('Counter render', () => {
 
     })
 
+    it('should render when `.reset` is called', () => {
+
+        const counter1 = new Counter()
+
+        counter1.reset() // second render
+
+        expect(counter1.render).toHaveBeenCalledTimes(2)
+
+    })
+
 })
 
-describe('DecreasingCounter render', () => {})
+describe('DecreasingCounter render', () => {
+
+    it('should render when `.dec` is called', () => {
+
+        const counter1 = new DecreasingCounter()
+
+        counter1.dec() // second render
+
+        expect(counter1.render).toHaveBeenCalledTimes(2)
+
+    })
+
+})
