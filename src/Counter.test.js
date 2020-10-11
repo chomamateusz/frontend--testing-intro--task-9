dae673e85a4d3a67297fcdfb05784031a7590403
Mateusz Choma <kontakt@coderoad.pl>
2020-10-11 18:30:41 +0200

    Tests added for 4, 5, 6 points form TODO

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -24,4 +24,61 @@ describe('Counter class tests', () => {
         expect(counter2.number).toBe(14)
     })
 
+    it('4.0. should have `.inc` method that bumps the stored number by `1`', () => {
+
+        const counter1 = new Counter()
+
+        expect(counter1.inc).toBeDefined()
+        expect(counter1.inc).toBeInstanceOf(Function)
+        expect(() => counter1.inc()).not.toThrow()
+        expect(counter1.number).toBe(1)
+
+    })
+
+    it('4.1. should have `.inc` method that bumps the stored number by `1` with starting value passed to constructor', () => {
+
+        const counter1 = new Counter(100)
+
+        counter1.inc()
+
+        expect(counter1.number).toBe(101)
+
+    })
+
+    it('5. should have `.dec` method that decreases the stored number by `1`', () => {
+
+        const counter1 = new Counter()
+
+        expect(counter1.dec).toBeDefined()
+        expect(counter1.dec).toBeInstanceOf(Function)
+        expect(() => counter1.dec()).not.toThrow()
+        expect(counter1.number).toBe(-1)
+
+    })
+
+    it('5.1. should have `.dec` method that decreases the stored number by `1` with starting value passed to constructor', () => {
+
+        const counter1 = new Counter(100)
+
+        counter1.dec()
+
+        expect(counter1.number).toBe(99)
+
+    })
+
+    it('6. should have `.reset` method overwrite that number to `0`', () => { 
+
+        const counter1 = new Counter(103)
+        counter1.inc() //104
+        counter1.inc() //105
+        counter1.dec() //104
+
+        expect(counter1.reset).toBeDefined()
+        expect(counter1.reset).toBeInstanceOf(Function)
+        expect(() => counter1.reset()).not.toThrow()
+
+        expect(counter1.number).toBe(0)
+
+    })
+
 })
